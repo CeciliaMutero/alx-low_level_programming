98d3908e7ace8c8bf2b32cfeca8898ea107f9608
CeciliaMutero <cecilmutero66@gmail.com>
2023-06-04 18:58:46 -0700

    Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * *leet -  Mozart composed his music not for the elite, but for everybody
+ * @str: string
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == 'a' || *ptr == 'A')
+
+			*ptr = '4';
+
+		else if (*ptr == 'e' || *ptr == 'E')
+
+			*ptr = '3';
+
+		else if (*ptr == 'o' || *ptr == 'O')
+
+			*ptr = '0';
+
+		else if (*ptr == 't' || *ptr == 'T')
+
+			*ptr = '7';
+
+		else if (*ptr == 'l' || *ptr == 'L')
+
+			*ptr = '1';
+
+		ptr++;
+	}
+
+	return (str);
+}
