b4d93f7f84e37d5b01f68dbc1233480542ace5ba
CeciliaMutero <cecilmutero66@gmail.com>
2023-06-21 16:40:29 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,6 +3,7 @@
  * int_index - searches for an integer.
  * @size: size of elements in array
  * @cmp: pointer to functionof one of 3 in main
+ *@array: array
  * Return: nothing
  */
 int int_index(int *array, int size, int (*cmp)(int))
@@ -13,8 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i])
-				return (i);
+		if (cmp(array[i]))
+			return (i);
 	}
 	return (-1);
 }
