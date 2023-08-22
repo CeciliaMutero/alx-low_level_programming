44be095847212c77003ad8e1e5e038c522f83161
CeciliaMutero <cecilmutero66@gmail.com>
2023-08-22 05:50:05 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * array_iterator -  function that executes a function given as a
  * parameter on each element of an array
