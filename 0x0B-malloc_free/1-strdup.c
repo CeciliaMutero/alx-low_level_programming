e618dfb6c4a91e08317026f5a5b19cd33370e5c9
Cecilia <cecilmutero66@gmail.com>
2023-10-03 07:56:35 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * _strdup - function that returns a pointer to a
+ * newly allocated space in memory
+ * @str: string to duplicate
+ *
+ * Return: Always 0 (success)
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	ptr = (char *)malloc(len * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
