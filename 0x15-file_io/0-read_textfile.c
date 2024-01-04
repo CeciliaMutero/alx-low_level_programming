f7a8ca261f7e3c49f4633644370cc0163c509748
Cecilia Mutero <cecilmutero66@gmail.com>
2024-01-04 15:29:54 -0800

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,49 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
 /**
- * read_textfile - function that reads a text file and prints it
+ * read_textfile - reads a text file and prints it
  * to the POSIX standard output
- * @filename: sting to be printed
- * @letters: number of letters in the string
- *
- * Return: number of letters
+ * @filename: pointer to the file
+ * @letters: number of letters it should read and print
+ * Return: Always 0 (success)
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *content;
-	ssize_t bytesread, byteswritten;
+	ssize_t bytesread;
+	ssize_t byteswritten;
+	char *space;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	content = malloc(sizeof(char) * letters);
-	if (content == NULL)
+	space = (char *)malloc(letters);
+	if (space == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	bytesread = read(fd, content, letters);
+	bytesread = read(fd, space, letters);
 	if (bytesread == -1)
 	{
+		free(space);
 		close(fd);
-		free(content);
 		return (0);
 	}
-	byteswritten = write(STDOUT_FILENO, content, bytesread);
-	if (byteswritten == -1 || byteswritten != bytesread)
+	byteswritten = write(STDOUT_FILENO, space, bytesread);
+	if (byteswritten == -1)
 	{
+		free(space);
 		close(fd);
-		free(content);
 		return (0);
 	}
+	free(space);
 	close(fd);
-	free(content);
-	return (byteswritten);
-	}
+	return (bytesread);
+}
